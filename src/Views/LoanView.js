9896965345014faa9c32d2ca2e8a68cfb87ac718
jaiswal_vikas <Vikas@802218>
2020-05-17 01:55:49 +0530

    Response data are showing

--- src/Views/LoanView.js
+++ src/Views/LoanView.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
-import {Card, Col, InputNumber, Layout, Row, Slider} from 'antd';
+import {Card, Col, InputNumber, Layout, message, Row, Slider} from 'antd';
 import {MenuFoldOutlined, MenuUnfoldOutlined,} from '@ant-design/icons';
 import axios from 'axios';
+
 const {Sider, Content, Header} = Layout;
 
 export default class LoanView extends Component {
@@ -12,9 +13,12 @@ export default class LoanView extends Component {
             interestAmount: null,
             collapsed: false,
             principal: 500,
-            duration:6
+            duration: 6,
+            interest:null,
+            emi:null
         }
-        this.checkKeyPress=this.checkKeyPress.bind(this);
+        this.checkKeyPress = this.checkKeyPress.bind(this);
+        this.apiCall = this.apiCall.bind(this);
     }
 
     toggle = () => {
@@ -23,23 +27,33 @@ export default class LoanView extends Component {
         });
     };
     onChange = value => {
-        this.setState({principal: value})
-        axios.get(`https://ftl-frontend-test.herokuapp.com/interest?amount=500&numMonths=12`).then(res => {
-                console.log(res.data);
-            })
+        this.setState({principal: value}, () => this.apiCall())
+
     };
     onMonthChange = value => {
-        this.setState({duration: value})
+        this.setState({duration: value}, () => this.apiCall())
     };
-    checkKeyPress(e){
-        if((e.keyCode >= 48 && e.keyCode <=57) || (e.keyCode >= 96 && e.keyCode <=105)) {
+
+    apiCall() {
+        let url = 'https://ftl-frontend-test.herokuapp.com/interest?amount=' + this.state.principal + '&numMonths=' + this.state.duration;
+        axios.get(url).then(res => {
+            console.log(res.data);
+            this.setState({interest:res.data.interestRate,emi:res.data.monthlyPayment.amount})
+
+        }).catch(err => {
+            message.error("Something went wrong");
+        })
+    }
+
+    checkKeyPress(e) {
+        if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
             console.log('number');
         }
 
     }
 
     render() {
-        let {principal,duration} = this.state;
+        let {principal, duration,emi,interest} = this.state;
 
         return <div><Layout>
             <Sider width={300} style={{height: '100vh'}} trigger={null} collapsible collapsed={this.state.collapsed}
@@ -62,7 +76,7 @@ export default class LoanView extends Component {
                                 style: {fontSize: '30px', margin: '10px 0 0 10px', float: 'left'}
                             })}
                         </div>
-                        <span style={{fontSize: '25px'}}>Lorem ipsum</span>
+                        <span style={{fontSize: '25px'}}>Loan Enquiry</span>
                     </div>
                     <br/>
                     <br/><br/>
@@ -71,8 +85,9 @@ export default class LoanView extends Component {
                         <Col xl={3} lg={3} md={24} sm={24} xs={24}>
                         </Col>
                         <Col xl={9} lg={9} md={24} sm={24} xs={24}>
-                            <Card title={<div style={{color: 'white', textAlign: 'center'}}><span
-                                style={{fontSize: '20px'}}>Input</span></div>} className={'innerCard'}
+                            <Card bodyStyle={{height: '200px'}}
+                                  title={<div style={{color: 'white', textAlign: 'center'}}><span
+                                      style={{fontSize: '20px'}}>Loan detail</span></div>} className={'innerCard'}
                                   headStyle={{borderBottom: '1px solid #33333d'}}>
                                 <span style={{color: 'white', fontSize: '20px'}}>Amount:</span>
                                 <Row>
@@ -83,15 +98,16 @@ export default class LoanView extends Component {
                                             min={500}
                                             max={5000}
                                             onChange={this.onChange}
-                                            value={typeof principal === 'number' ? principal  : 0}
+                                            value={typeof principal === 'number' ? principal : 0}
                                         />
                                     </Col>
                                     <Col xl={10} lg={10} md={24} sm={24} xs={24}>
-                                        <span style={{color:'white',fontSize:'20px',marginLeft:'10px'}}>$<InputNumber
+                                        <span
+                                            style={{color: 'white', fontSize: '20px', marginLeft: '10px'}}>$<InputNumber
                                             type={"number"}
                                             min={500}
                                             max={5000}
-                                            style={{margin: '0 5px',width:'60px'}}
+                                            style={{margin: '0 5px', width: '60px'}}
                                             value={principal}
                                             onChange={this.onChange}
                                         /></span>
@@ -111,11 +127,11 @@ export default class LoanView extends Component {
                                         />
                                     </Col>
                                     <Col xl={10} lg={10} md={24} sm={24} xs={24}>
-                                         <InputNumber
+                                        <InputNumber
                                             type={"number"}
                                             min={6}
                                             max={24}
-                                            style={{margin: '0 25px',width:'60px'}}
+                                            style={{margin: '0 25px', width: '60px'}}
                                             value={duration}
                                             onChange={this.onMonthChange}
                                         />
@@ -124,14 +140,39 @@ export default class LoanView extends Component {
 
                             </Card>
                         </Col>
-                        <Col  xl={9} lg={9} md={24} sm={24} xs={24}>
-                            <Card title={<div style={{color: 'white', textAlign: 'center'}}><span
-                                style={{fontSize: '20px'}}>Output</span></div>} className={'innerCard'}
+                        <Col xl={9} lg={9} md={24} sm={24} xs={24}>
+                            <Card bodyStyle={{height: '200px'}}
+                                  title={<div style={{color: 'white', textAlign: 'center'}}><span
+                                      style={{fontSize: '20px'}}>Interest detail</span></div>} className={'innerCard'}
                                   headStyle={{borderBottom: '1px solid #33333d'}}>
+                                <br/>
 
+                                <Row>
+                                    <Col xl={14} lg={14} md={24} sm={24} xs={24}>
+                                        <span style={{
+                                            color: 'white',
+                                            fontSize: '30px',
+                                            fontStyle: 'italic'
+                                        }}>Interest: {interest}</span>
+                                    </Col>
+                                    <Col xl={10} lg={10} md={24} sm={24} xs={24}>
+                                    </Col>
+                                </Row>
+
+
+                                <Row>
+                                    <Col xl={14} lg={14} md={24} sm={24} xs={24}>
+                                        <span
+                                            style={{color: 'white', fontSize: '30px', fontStyle: 'italic'}}>EMI:{emi}</span>
+
+                                    </Col>
+                                    <Col xl={10} lg={10} md={24} sm={24} xs={24}>
+
+                                    </Col>
+                                </Row>
                             </Card>
                         </Col>
-                        <Col  xl={3} lg={3} md={24} sm={24} xs={24}>
+                        <Col xl={3} lg={3} md={24} sm={24} xs={24}>
                         </Col>
                     </Row>
                 </Content>
